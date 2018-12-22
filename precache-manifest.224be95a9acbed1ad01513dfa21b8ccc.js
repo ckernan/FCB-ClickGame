6da5509334485d3e7bcff2222d8fc7a3d05894da
Cristina Kernan <41130164+ckernan@users.noreply.github.com>
2018-12-21 18:05:08 -0600

    Updates

---
+++ precache-manifest.224be95a9acbed1ad01513dfa21b8ccc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "89e971b5cf5bd9233f89",
+    "url": "/FCB-ClickGame/static/js/runtime~main.89e971b5.js"
+  },
+  {
+    "revision": "3415578716e49baa3592",
+    "url": "/FCB-ClickGame/static/js/main.34155787.chunk.js"
+  },
+  {
+    "revision": "4a4cf2359ecd8a3b1537",
+    "url": "/FCB-ClickGame/static/js/1.4a4cf235.chunk.js"
+  },
+  {
+    "revision": "3415578716e49baa3592",
+    "url": "/FCB-ClickGame/static/css/main.1b54be57.chunk.css"
+  },
+  {
+    "revision": "5a5b99ccf028bb5ea3dce207e5a15a62",
+    "url": "/FCB-ClickGame/index.html"
+  }
+];
